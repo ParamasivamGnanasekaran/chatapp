c3d5f4a0f368f2ff9e4df59a30785b0b3434ea31
paramasivamg <paramasivam.g@solitontech.com>
2022-10-17 19:04:28 +0530

    initial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-users = [];
+let users = [];
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/chatapp/index.html');
@@ -45,7 +45,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT, () => {
   console.log('listening on :3000');
 });
 
